c9d5240504ddf18cb80b98e8edb0f89817ae5a62
Hitesh <Hiteshrew123@gmail.com>
2020-02-18 13:54:56 +0530

    login-signup created

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,5 +8,6 @@ class UserCreateForm(UserCreationForm):
         model = get_user_model()
 
     def __init__(self,*args,**kwargs):
-        super().__init__(*arhs,**kwargs)
+        super().__init__(*args,**kwargs)
         self.fields['username'].label = 'Display Name'
+        self.fields['email'].label = 'Email Addresss'
--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts'
 ]
 
@@ -120,3 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
