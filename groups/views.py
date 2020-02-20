e1dc89293c7bb29104b378d81a1ad7c226375ec0
Hitesh <Hiteshrew123@gmail.com>
2020-02-20 11:41:45 +0530

    PART 9

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+
+from django.core.urlresolvers import reverse
+
+from django.views import generic
+from groups.models import Group,GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+	fields = ('name','description')
+	model = Group
+
+
+class SingleGroup(generic.DetailView):
+	model = Group
+
+class ListGroups(generic.ListView):
+	model = Group
+
